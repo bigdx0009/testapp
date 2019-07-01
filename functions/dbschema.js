d7ec773a89de7fa7d5cd608d545689d172e60349
bigdx0009 <bigdx009@gmail.com>
2019-07-01 11:10:03 -0400

    inital commit

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+	screams: [
+		{
+			userHandle: 'user',
+			body: 'this is the scream body',
+			createdAt: '2019-07-01T03:14:04.456Z',
+			likeCount: 5,
+			commentCount: 2
+		}
+	]
+}
---
+++ functions/index.js
@@ -0,0 +1,93 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const app = require('express')();
+
+admin.initializeApp();
+
+var config = {
+	apiKey: "AIzaSyBGF64c7RECuOwJgfG62UpRZMJYtv3mmIg",
+  	authDomain: "test-project-81890.firebaseapp.com",
+  	databaseURL: "https://test-project-81890.firebaseio.com",
+  	projectId: "pest-project-81890",
+  	storageBucket: "test-project-81890.appspot.com",
+  	messagingSenderId: "233571820565",
+  	appId: "1:233571820565:web:b572dc6b9a3a64c9"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
+app.get('/screams',(req, res) => {
+	db
+		.collection('screams')
+		.orderBy('createdAt', 'desc')
+		.get().then(data => {
+		let screams = [];
+		data.forEach(doc => {
+			screams.push({
+				screamID: doc.id,
+				body: doc.data().body,
+				userHandle: doc.data().userHandle,
+				createdAt: doc.data().createdAt,
+				commentCount: doc.data().commentCount,
+				likeCount: doc.data().likeCount
+			})
+		});
+		return res.json(screams);
+	})
+	.catch(err => console.error(err));
+});
+
+app.post('/scream',(req, res) => {
+	const newScream = {
+		body: req.body.body,
+		userHandle: req.body.userHandle,
+		createdAt: new Date().toISOString()
+	};
+
+	db
+		.collection('screams')
+		.add(newScream)
+		.then(doc => {
+			res.json({message: `document ${doc.id} created successfully`});
+		})
+
+	.catch(err => {
+		res.status(500).json({error: 'somthing went wrong'});
+		console.error(err);
+	});
+});
+
+app.post('/signup', (req, res) => {
+	const newUser = {
+		email: req.body.email,
+		password: req.body.password,
+		confirmPassword: req.body.confirmPassword,
+		handle: req.body.handle,
+	};
+
+db.doc(`/users/${newUser.handle}`).get()
+	.then(doc => {
+		if(doc.exsist){
+			return res.status(400).json({ handle: 'this handle is already taken'});
+		}else {
+			return firebase
+				.auth()
+				.createUserWithEmailAndPassword(newUser.email, newUser.password)
+		}
+})
+	.then(data => {
+	return data.user.getIdToken();
+})
+	.then(token => {
+	return res.status(201).json({ token });
+})
+	.catch(err => {
+		console.error(err);
+		return res.status(500).json({ error: err.code});
+	});
+})
+
+exports.api = functions.region('us-east1').https.onRequest(app);
