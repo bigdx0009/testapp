8e92e4ba83898d94dbb11cfbc3bdbb5054149b93
bigdx0009 <bigdx009@gmail.com>
2019-07-01 19:36:47 -0400

    fixed error

--- functions/index.js
+++ functions/index.js
@@ -63,8 +63,8 @@ app.post('/scream',(req, res) => {
 const isEmail = (email) => {
 	const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	if(email.match(regEx)) return true;
-}else return false;
-
+ else return false;
+}
 const isEmpty = (string) => {
 	if(string.trim() === '') return true;
 	else return false;
